899ac992db70bbc40a3653e8829c036ecdcac0b5
Aleksander Ciesiołkiewicz <a.ciesiolkiewicz@gmail.com>
2018-07-08 22:56:24 +0200

    Configure script that prepares package for distribution

--- index.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/CommandBus';
-export * from './src/EventBus';
-export * from './src/MessageBus';
-export * from './src/Middleware';
-export * from './src/MessageHandler';
-export * from './src/Message';
-
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './CommandBus';
+export * from './EventBus';
+export * from './MessageBus';
+export * from './Middleware';
+export * from './MessageHandler';
+export * from './Message';
+
